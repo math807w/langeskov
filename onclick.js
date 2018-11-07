66e8f53f8873dc959ca5ddae0ce585145b563fc4
sofie florczak <sofi0143@edu.eal.dk>
2018-11-07 09:07:48 +0100

    facebook peekaboo

--- onclick.js
+++ onclick.js
@@ -3,9 +3,13 @@ function toggle_div_fun(id) {
 
        var divelement = document.getElementById(id);
 
-       if(divelement.style.display == 'none')
+       if(divelement.style.display == 'none'){
           divelement.style.display = 'block';
-       else
+	      document.getElementById("rotate").style.right = "358px";}
+       else{
           divelement.style.display = 'none';
-    }
+		  document.getElementById("rotate").style.right = "0px";
+    } }
+
+
 //skriv noter i alle dokumenter til hvad der reelt skal gøres for at implementere peekabooen rigtigt) 
